847cdee08b025aa9f5799bc23c0449d172d8f7a2
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2024-09-29 21:11:50 -0300

    FEAT Adicao do componente de componentes planejados

---
+++ src/app/_components/struct/struct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-struct',
+  templateUrl: './struct.component.html',
+  styleUrl: './struct.component.css'
+})
+export class StructComponent {
+
+}
