b788d1b7ebff577653c40f113f79a899275d2a99
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2024-10-06 16:50:16 -0300

    FEAT Obter dados do saldo da conta

--- src/app/_components/eye-button/eye-button.component.ts
+++ src/app/_components/eye-button/eye-button.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RendaFixaService } from '../../services/rendafixa.service';
+import { GetContaByIdResponse } from '../../models/conta.model';
 
 @Component({
   selector: 'app-eye-button-disponivel',
   templateUrl: './eye-button.component.html',
 })
 export class EyeButtonComponent {
+  @Input() saldo: number | undefined; 
   mostrarSaldo: boolean = false;
 
   alternarVisibilidade() {
     this.mostrarSaldo = !this.mostrarSaldo;
+  }
+  
+  getSaldo(): string {
+    return this.saldo? "R$ "+this.saldo.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.') : "R$ 0,00";
   }
 }
