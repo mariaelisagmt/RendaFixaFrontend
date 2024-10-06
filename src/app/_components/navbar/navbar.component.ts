610b54a3d57c856f096d05a57704b88f20658da9
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2024-10-06 18:43:58 -0300

    FEAT Mostrar nome e dados da conta

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
-
+  @Input() nome: string | undefined;
+  
+  obterNomeCliente(): string {
+    return this.nome? "Olá "+ this.nome : "Olá, Cliente";
+  }
 }
