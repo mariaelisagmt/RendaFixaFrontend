9499e399dfb51b1392557d5111886447f212d3ba
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2024-10-06 17:18:03 -0300

    FIX Atualizando visualizar saldo

--- src/app/_components/eye-button/eye-button.component.ts
+++ src/app/_components/eye-button/eye-button.component.ts
@@ -1,20 +1,19 @@
 import { Component, Input } from '@angular/core';
-import { RendaFixaService } from '../../services/rendafixa.service';
-import { GetContaByIdResponse } from '../../models/conta.model';
 
 @Component({
   selector: 'app-eye-button-disponivel',
   templateUrl: './eye-button.component.html',
 })
+
 export class EyeButtonComponent {
   @Input() saldo: number | undefined; 
   mostrarSaldo: boolean = false;
+  
+  getSaldo(): string {
+    return this.saldo? "R$ " + this.saldo.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.') : "R$ 0,00";
+  }
 
   alternarVisibilidade() {
     this.mostrarSaldo = !this.mostrarSaldo;
-  }
-  
-  getSaldo(): string {
-    return this.saldo? "R$ "+this.saldo.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.') : "R$ 0,00";
   }
 }
